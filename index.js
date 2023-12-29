fcbcabfed65552f6f3ccaf9be047f063e7f15293
JacobKnaack <jacobknck@gmail.com>
2023-12-29 15:46:55 -0800

    adds auth, Collection interface connections

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 require('dotenv').config();
 const server = require('./lib/server');
+const { init } = require('./lib/model');
 
 const PORT = process.env.PORT || 3001;
 
-server.start(PORT);
+init().then(() => {
+  server.start(PORT);
+});
